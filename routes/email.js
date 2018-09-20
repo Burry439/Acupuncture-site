9602d0d92e6ad24e8ae33bc2b1b725902ba15d16
burry <burry439@gmail.com>
2018-09-20 18:50:07 +0300

    d

--- routes/email.js
+++ routes/email.js
@@ -23,11 +23,11 @@ router.get('/',(req,res)=>{
   
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"the dude👻" <burry439@gmail.com>', // sender address
+        from: '"the dudeeee👻" <furrur439@gmail.com>', // sender address
         to: 'burry439@gmail.com', // list of receivers
         subject: 'Hello ✔', // Subject line
         text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
+        html: '<b>Hello world yessss?</b>' // html body
     };
   
     // send mail with defined transport object
