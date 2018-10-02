a9c519073d1ff5149c5fb2ad1310777d043131f0
burry <burry439@gmail.com>
2018-10-02 17:27:59 +0300

    dd

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,6 +4,9 @@ const Appointment = require('../models/appointment')
 const nodemailer = require('nodemailer');
 
 const sgMail = require('@sendgrid/mail');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendConfirmationEmail = (appointment)=>{
@@ -23,9 +26,20 @@ const sendConfirmationEmail = (appointment)=>{
         from : appointment.name + '     ' + appointment.email, // sender address
         to: 'burry439@gmail.com', // list of receivers
         subject: 'New Appointment', // Subject line
-        text: 'Name:' +appointment.name + 'Treatment Type: ' +appointment.title + 'Phone Number: ' +appointment.phone + 'Email: ' +appointment.email, // plain text body
-        html: ' <br> Name:' +appointment.name + '<br> Phone Number: ' +appointment.phone + '<br> Email: ' +appointment.email + '<br>' + '<br> Treatement Type: ' + appointment.title + '<br>'// html body
+        text: 'Name:' +appointment.name + 'Treatment Type: ' +appointment.title + 'Phone Number: ' +appointment.phone + 'Email: ' +appointment.email + ' Time ' + appointment.start , // plain text body
+        html: ' <br> Name:' +appointment.name + '<br> Phone Number: ' +appointment.phone + '<br> Email: ' +appointment.email + '<br>' + 'Treatement Type: ' + appointment.title + '<br>' + ' Time: ' + appointment.start + '<br>'// html body
     };
+
+
+    let mailOptions2 = {
+        from : 'burry439@gmail.com'+ '     ' + 'Burry The Guy In Charge', // sender address
+        to: appointment.email, // list of receivers
+        subject: 'Appointment Details', // Subject line
+        text: 'Hey ' +appointment.name + ' We Got your Request For ' +appointment.title + ' On ' + appointment.start + ' Will See you soon', // plain text body
+        html: ' <br>' +  'Hey ' +appointment.name + ' We Got your Request For ' +appointment.title + ' On ' + appointment.start + ' Will See you soon'   + '<br>'// html body
+    };
+
+
   
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
@@ -34,9 +48,16 @@ const sendConfirmationEmail = (appointment)=>{
         } 
         console.log('Message sent: %s', info.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        res.json("heyo")
     });
 
+
+    transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) {
+            return console.log("ERRORRRRR " + error);
+        } 
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
 
 }
 
@@ -51,10 +72,23 @@ const sendConfirmationEmail = (appointment)=>{
 
 router.post('/newAppointement', (req,res)=>{
     console.log(req.body)
+
+
+    const date = new Date(req.body.start)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day =  date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+
+
+    theDate = months[month] +' ' + day + ' ' + year + ' At: ' + hour +":" + minute
+
+
     const appointment = new Appointment
     ({
         title: req.body.title,
-        start: req.body.start,
+        start: theDate,
         name : req.body.name,
         email: req.body.email,
         phone: req.body.phone
--- routes/email.js
+++ routes/email.js
@@ -28,18 +28,38 @@ router.put('/', (req,res)=>{
         text: 'Name:' + req.body.name + 'Message:' + req.body.msg + 'Phone Number: ' + req.body.phone + 'Email: ' + req.body.email, // plain text body
         html: ' <br> Name:' + req.body.name + '<br> Phone Number: ' + req.body.phone + '<br> Email: ' + req.body.email + '<br>' + '<br> Message:' + req.body.msg + '<br>'// html body
     };
+
+
+    let mailOptions2 = {
+        from: 'Burry the guy in charge' + ' ' + 'burry439@gmail.com', // sender address
+        to: req.body.email , // list of receivers
+        subject: 'Message Recived', // Subject line
+        text: 'Hey' + req.body.name + 'We got your message and will reply to you shortly', // plain text body
+        html: ' <br>' + 'Hey ' + req.body.name + ' We got your message and will reply to you shortly' + '<br>'// html body
+    };
   
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
+        if (error) { 
             return console.log("ERRORRRRR " + error);
         } 
         console.log('Message sent: %s', info.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        res.json("heyo")
+     
     });
    
+    transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) { 
+            return console.log("ERRORRRRR " + error);
+        } 
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+     
+    });
+
    
+    res.json("Done")
+
 })
 
 
